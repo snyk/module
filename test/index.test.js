4a1055822a33b4294bd28e3502135e1153c06a46
Remy Sharp <remy@remysharp.com>
2015-08-13 12:34:01 +0100

    feat: working with tests

---
+++ test/index.test.js
@@ -0,0 +1,12 @@
+var tape = require('tape');
+var mod = require('../');
+
+tape('module string to object', function (t) {
+  t.deepEqual(mod('nodemon'), { name: 'nodemon', version: '*' }, 'supports versionless');
+  t.deepEqual(mod('nodemon@1'), { name: 'nodemon', version: '1' }, 'with version');
+  t.throws(function () { mod('@remy/snyk-module'); }, /not supported: private module/, 'private not supported');
+  t.throws(function () { mod('grunt-sails-linker@git://github.com/Zolmeister/grunt-sails-linker.git'); }, /not supported: external module/, 'external not supported');
+  t.throws(function () { return mod('ikt@git+http://ikt.pm2.io/ikt.git#master'); }, /not supported: external module/, 'external not supported');
+
+  t.end();
+});
