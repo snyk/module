e93ff2793847f6537d31135715cf2fcae3f2f1db
Remy Sharp <remy@remysharp.com>
2016-01-29 18:32:02 +0000

    feat: support github/lab and bitbucket

--- test/index.test.js
+++ test/index.test.js
@@ -1,17 +1,61 @@
-var tape = require('tape');
+var test = require('tap').test;
 var mod = require('../');
 
-tape('module string to object', function (t) {
+test('module string to object', function (t) {
   t.deepEqual(mod('nodemon'), { name: 'nodemon', version: '*' }, 'supports versionless');
   t.deepEqual(mod('nodemon@1'), { name: 'nodemon', version: '1' }, 'with version');
-  // t.throws(function () { mod('@remy/snyk-module'); }, /not supported: private module/, 'private not supported');
-  t.throws(function () { mod('grunt-sails-linker@git://github.com/Zolmeister/grunt-sails-linker.git'); }, /not supported: external module/, 'external not supported');
-  t.throws(function () { return mod('ikt@git+http://ikt.pm2.io/ikt.git#master'); }, /not supported: external module/, 'external not supported');
+  t.deepEqual(mod('@remy/snyk-module'), { name: '@remy/snyk-module', version: '*' }, 'private packages');
+  t.deepEqual(mod('jsbin/jsbin'), { name: 'jsbin', version: 'jsbin/jsbin' }, 'short github works');
+
+
+  var urls = [
+    'https://github.com/remy/undefsafe',
+    'https://github.com/remy/undefsafe/',
+    'https://github.com/remy/undefsafe.git',
+    'git@github.com:remy/undefsafe.git',
+    'git@bitbucket.org:remy/undefsafe.git',
+    'remy/undefsafe',
+  ];
+
+  var expect = {
+    name: 'undefsafe',
+    version: 'remy/undefsafe',
+  };
+
+  urls.forEach(function (url) {
+    t.deepEqual(mod(url), expect, 'short github works');
+  });
+
+  t.deepEqual(mod(urls[0] + '#123'), { name: 'undefsafe', version: 'remy/undefsafe#123'}, 'add hash correctly');
+
+  t.throws(function () {
+    mod('/');
+  }, /supported: invalid package name/, 'catch invalid package name');
+
+  t.throws(function () {
+    mod('  *');
+  }, /supported: invalid package name/, 'catch invalid package name');
+
+
+  t.throws(function () {
+    mod();
+  }, /requires string/, 'requires args');
+
+
+  // usernames on git urls aren't supported (unsure if this is 100% right tho)
+  t.throws(function () {
+    mod('grunt-sails-linker@git://github.com/Zolmeister/grunt-sails-linker.git');
+  }, /not supported: external module/, 'external not supported');
+
+  // privately hosted git repo not supported
+  t.throws(function () {
+    mod('ikt@git+http://ikt.pm2.io/ikt.git#master');
+  }, /not supported: external module/, 'external not supported');
 
   t.end();
 });
 
-tape('encoding', function (t) {
+test('encoding', function (t) {
   t.equal(mod.encode('snyk'), 'snyk', 'vanilla strings unaffected');
   t.equal(mod.encode('@snyk/config'), '@snyk%2Fconfig', 'slash is escaped');
   t.end();
